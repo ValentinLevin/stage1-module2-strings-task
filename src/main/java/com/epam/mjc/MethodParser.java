64f5e6083ed7942b000162ba5171123de28b5ab7
Valentin Levin <levin_valentin@mail.ru>
2023-12-20 19:47:26 +0600

    solution #1

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,7 +1,8 @@
 package com.epam.mjc;
 
-public class MethodParser {
+import java.util.List;
 
+public class MethodParser {
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
@@ -20,6 +21,56 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (signatureString == null || signatureString.isEmpty()) {
+            throw new IllegalArgumentException("The value of SignatureString is wrong (input value is empty)");
+        }
+
+        int argumentsStartIndex = signatureString.indexOf("(");
+        int argumentsFinishIndex = signatureString.indexOf(")");
+
+        if (argumentsStartIndex == -1 || argumentsFinishIndex == -1 || argumentsStartIndex > argumentsFinishIndex) {
+            throw new IllegalArgumentException("The value of SignatureString is wrong (argument braces not found)");
+        }
+
+        MethodSignature methodSignature = getMethodSignature(signatureString, argumentsStartIndex);
+        parseAndLoadMethodArguments(signatureString, argumentsStartIndex, argumentsFinishIndex, methodSignature.getArguments());
+
+        return methodSignature;
+    }
+
+    private MethodSignature getMethodSignature(String signatureString, int argumentsStartIndex) {
+        String signatureStringWithoutArguments = signatureString.substring(0, argumentsStartIndex);
+
+        String[] methodElementsWithoutArguments = signatureStringWithoutArguments.split("( +)");
+        if (methodElementsWithoutArguments.length < 2) {
+            throw new IllegalArgumentException("The value of SignatureString is wrong (not have method name or return type)");
+        }
+        String methodName = methodElementsWithoutArguments[methodElementsWithoutArguments.length-1];
+
+        MethodSignature methodSignature = new MethodSignature(methodName);
+        methodSignature.setReturnType(methodElementsWithoutArguments[methodElementsWithoutArguments.length-2]);
+
+        if (methodElementsWithoutArguments.length > 2) {
+            methodSignature.setAccessModifier(methodElementsWithoutArguments[methodElementsWithoutArguments.length - 3]);
+        }
+        return methodSignature;
+    }
+
+    private void parseAndLoadMethodArguments(
+            String signatureString, int argumentsStartIndex, int argumentsFinishIndex, List<MethodSignature.Argument> list
+    ) {
+        String argumentsString = signatureString.substring(argumentsStartIndex + 1, argumentsFinishIndex);
+
+        if (!argumentsString.isEmpty()) {
+            String[] arguments = argumentsString.split(",");
+            for (String argumentString : arguments) {
+                String[] argumentParts = argumentString.trim().split("( +)");
+                if (argumentParts.length != 2) {
+                    throw new IllegalArgumentException("The value of SignatureString is wrong (incorrect arguments descriptions)");
+                }
+                MethodSignature.Argument argument = new MethodSignature.Argument(argumentParts[0], argumentParts[1]);
+                list.add(argument);
+            }
+        }
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,57 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class StringSplitter {
-
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
      * @param source source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (delimiters == null || delimiters.isEmpty()) {
+            throw new IllegalArgumentException("Delimiters list is empty");
+        }
+
+        if (source == null) {
+            throw new IllegalArgumentException("Source string not specified");
+        }
+
+        List<String> list = new ArrayList<>();
+        int fromIndex = 0;
+        int nextIndex;
+        String nextDelimiter;
+        do {
+            nextDelimiter = findNextDelimiter(source, fromIndex, delimiters);
+            if (nextDelimiter == null) {
+                nextIndex = source.length();
+            } else {
+                nextIndex = source.indexOf(nextDelimiter, fromIndex);
+            }
+            if (fromIndex != nextIndex) {
+                list.add(source.substring(fromIndex, nextIndex));
+            }
+            if (nextDelimiter != null) {
+                fromIndex = nextIndex + nextDelimiter.length();
+            }
+        } while (nextDelimiter != null);
+
+        return list;
+    }
+
+    private String findNextDelimiter(String source, int fromIndex, Collection<String> delimiters) {
+        int nextIndex = -1;
+        String nextDelimiter = null;
+        for (String delimiter: delimiters) {
+            int index = source.indexOf(delimiter, fromIndex);
+            if (index > -1 && (nextIndex == -1 || index < nextIndex)) {
+                nextIndex = index;
+                nextDelimiter = delimiter;
+            }
+        }
+
+        return nextDelimiter;
     }
 }
